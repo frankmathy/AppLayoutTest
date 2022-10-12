18791deade4f7bb1bcf09ae3dbc8393c146f39c5
Frank Mathy <frankmathy@gmail.com>
2022-10-13 00:09:21 +0200

    Layout achieved

--- src/RateGrid.js
+++ src/RateGrid.js
@@ -4,10 +4,9 @@ import RatePanel from './RatePanel'
 const RateGrid = () => {
     return (
         <div className="RateGrid">
-              <RatePanel ccyPair='Hey'/>
-        {
-        Array.from(Array(300).keys()).map(ccyPair => <RatePanel ccyPair='EUR/USD'/>)
-        }
+          {
+            Array.from(Array(300).keys()).map(ccyPair => <RatePanel ccyPair='EUR/USD'/>)
+          }
       </div>
     )
 }
