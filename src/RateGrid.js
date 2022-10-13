4222a80eaed2ad720f5fdf975f6b926ecc868ce9
Frank Mathy <frankmathy@gmail.com>
2022-10-13 18:58:25 +0200

    Final touches

--- src/RateGrid.js
+++ src/RateGrid.js
@@ -4,8 +4,8 @@ import DataElement from "./DataElement";
 const RateGrid = () => {
   return (
     <div className="RateGrid">
-      {Array.from(Array(300).keys()).map((no) => (
-        <DataElement text={`Rate ${no}`} />
+      {Array.from(Array(50).keys()).map((no) => (
+        <DataElement text={`Rate ${no + 1}`} />
       ))}
     </div>
   );
--- src/TradesList.js
+++ src/TradesList.js
@@ -5,7 +5,7 @@ const TradesList = () => {
   return (
     <div className="TradesList">
       {Array.from(Array(50).keys()).map((no) => (
-        <DataElement text={`Trade ${no}`} />
+        <DataElement text={`Trade ${no + 1}`} />
       ))}
     </div>
   );
